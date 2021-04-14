9b5182ec73cdc8adf71ca58aa7d361dcb19ca5fc
ochi <mune.ochi@gmail.com>
2021-04-15 00:34:18 +0900

    implement the first screen and the touch event

--- app/src/main/java/com/example/catchtheball/MainActivity.java
+++ app/src/main/java/com/example/catchtheball/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.catchtheball;
 
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private TextView scoreLabel;
+    private TextView startLabel;
+    private ImageView box;
+    private ImageView orange;
+    private ImageView pink;
+    private ImageView black;
+
+    // 青いボックスの位置
+    private float boxY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scoreLabel = findViewById(R.id.scoreLabel);
+        startLabel = findViewById(R.id.startLabel);
+        box = findViewById(R.id.box);
+        orange = findViewById(R.id.orange);
+        pink = findViewById(R.id.pink);
+        black = findViewById(R.id.black);
+
+        // 始めは画面の外に置いておく
+        orange.setX(-80.0f);
+        orange.setY(-80.0f);
+        pink.setX(-80.0f);
+        pink.setY(-80.0f);
+        black.setX(-80.0f);
+        black.setY(-80.0f);
+
+        startLabel.setVisibility(View.INVISIBLE);
+        boxY = 500.0f;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            boxY -= 20;
+        }
+        box.setY(boxY);
+        return true;
     }
 }
