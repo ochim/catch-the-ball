0493b53c582491e8516ff69833734379604ea3be
ochi <mune.ochi@gmail.com>
2021-04-15 19:42:00 +0900

    implement the movements of the balls

--- app/src/main/java/com/example/catchtheball/MainActivity.java
+++ app/src/main/java/com/example/catchtheball/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.catchtheball;
 
+import android.graphics.Point;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -25,9 +28,17 @@ public class MainActivity extends AppCompatActivity {
     // サイズ
     private int frameHeight;
     private int boxSize;
+    private int screenWidth;
+    private int screenHeight;
 
-    // 青いボックスの位置
+    // ボックスやボールの位置
     private float boxY;
+    private float orangeX;
+    private float orangeY;
+    private float pinkX;
+    private float pinkY;
+    private float blackX;
+    private float blackY;
 
     // Handler & Timer
     private Handler handler = new Handler();
@@ -49,6 +60,13 @@ public class MainActivity extends AppCompatActivity {
         pink = findViewById(R.id.pink);
         black = findViewById(R.id.black);
 
+        WindowManager wm = getWindowManager();
+        Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        screenWidth = size.x;
+        screenHeight = size.y;
+
         // 始めは画面の外に置いておく
         orange.setX(-80.0f);
         orange.setY(-80.0f);
@@ -59,6 +77,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void changePos() {
+        // Orange
+        orangeX -= 12;
+        if (orangeX < 0) {
+            orangeX = screenWidth + 20;
+            orangeY = (float)Math.floor(Math.random() * (frameHeight - orange.getHeight()));
+        }
+        orange.setX(orangeX);
+        orange.setY(orangeY);
+
+        // Black
+        blackX -= 16;
+        if (blackX < 0) {
+            blackX = screenWidth + 10;
+            blackY = (float)Math.floor(Math.random() * (frameHeight - black.getHeight()));
+        }
+        black.setX(blackX);
+        black.setY(blackY);
+
+        // Pink
+        pinkX -= 20;
+        if (pinkX < 0) {
+            pinkX = screenWidth + 5000;
+            pinkY = (float)Math.floor(Math.random() * (frameHeight - pink.getHeight()));
+        }
+        pink.setX(pinkX);
+        pink.setY(pinkY);
+
         if (actionFlg) {
             boxY -= 20;
         } else {
