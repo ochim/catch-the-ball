1b60c04e1a3a8857f5bbf80c5e2fcd8958b6661f
ochi <mune.ochi@gmail.com>
2021-04-15 22:08:22 +0900

    implement the start screen

---
+++ app/src/main/java/com/example/catchtheball/StartActivity.java
@@ -0,0 +1,20 @@
+package com.example.catchtheball;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+    public void startGame(View view) {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
