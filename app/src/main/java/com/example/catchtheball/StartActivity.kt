d2acf4978e9af2caea27725d9c59ca7a48729e2e
ochi <mune.ochi@gmail.com>
2022-01-14 18:06:29 +0900

    implement test adview SMART_BANNER of AdMob

--- app/src/main/java/com/example/catchtheball/StartActivity.kt
+++ app/src/main/java/com/example/catchtheball/StartActivity.kt
@@ -4,12 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 
 class StartActivity : AppCompatActivity() {
+    lateinit var mAdView : AdView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+        MobileAds.initialize(this) {}
+
+        mAdView = this.findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
     }
 
     fun startGame(view: View?) {
